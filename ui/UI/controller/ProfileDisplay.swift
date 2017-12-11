7251684abb3b6bafc1f13cdd9174cf8485dd3bf4
Test Person <Test@ip0af540a6.int.uvm.edu>
2017-12-11 17:19:50 -0500

    some profile updates

--- ui/UI/controller/ProfileDisplay.swift
+++ ui/UI/controller/ProfileDisplay.swift
@@ -5,10 +5,11 @@
 //  Created by MN Team on 10/16/17.
 //  Copyright © 2017 UVM CEMS All rights reserved.
 //
-
 import UIKit
 
-class ProfileDisplay: BaseVC {
+class ProfileDisplay: BaseVC, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
     
     @IBOutlet weak var name: UILabel!
     @IBOutlet weak var email: UILabel!
@@ -28,6 +29,10 @@ class ProfileDisplay: BaseVC {
     @IBOutlet weak var editInstruments: UITextField!
     @IBOutlet weak var editBio: UITextField!
     
+    @IBOutlet weak var update: UIButton!
+    @IBOutlet weak var editprof: UIButton!
+    
+    @IBOutlet weak var editProPic: UIButton!
     @IBOutlet weak var profilePic: UIImageView!
     
     
@@ -43,6 +48,20 @@ class ProfileDisplay: BaseVC {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        name.textAlignment = .center
+        imagePicker.delegate = self
+        
+        editProPic.isHidden = true
+        editprof.layer.borderWidth=2.0
+        editprof.layer.cornerRadius = 5
+        editprof.layer.borderWidth = 1
+        editprof.layer.borderColor=UIColor.lightGray.cgColor
+       
+        update.layer.borderWidth=2.0
+        update.layer.cornerRadius = 5
+        update.layer.borderWidth = 1
+        update.layer.borderColor=UIColor.lightGray.cgColor
         self.navigationController?.setNavigationBarHidden(true, animated: true)
         
         
@@ -72,13 +91,40 @@ class ProfileDisplay: BaseVC {
         editRole.isHidden = true
         editBio.isHidden = true
         
+        update.isHidden = true
+        
         self.profilePic.layer.cornerRadius = self.profilePic.frame.size.width/2;
         profilePic.clipsToBounds = true;
+        profilePic.layer.borderWidth = 4
+        profilePic.layer.borderColor = UIColor.black.cgColor
         
         name.textAlignment = NSTextAlignment.center;
     }
     
+    @IBAction func getUserPhoto(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker,animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: AnyObject])
+    {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        {
+            profilePic.contentMode = .scaleAspectFit
+            profilePic.image = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBAction func editProfile(_ sender: Any) {
+        update.isHidden = false
+        editProPic.isHidden = false
+        
+        name.textAlignment = .left
+        
         editName.text = name.text
         editEmail.text = email.text
         editLocation.text = location.text
@@ -116,17 +162,18 @@ class ProfileDisplay: BaseVC {
         
     }
     @IBAction func updateProfile(_ sender: Any) {
+        
         editName.isHidden = true
         editEmail.isHidden = true
         editLocation.isHidden = true
         editPhone.isHidden = true
         editRole.isHidden = true
         editBio.isHidden = true
+        editProPic.isHidden = true
         
-        name.textAlignment = NSTextAlignment.center;
+        name.textAlignment = .center;
         name.font = name.font.withSize(25);
 
-        
         name.text = editName.text
         email.text = editEmail.text
         location.text = editLocation.text
@@ -134,16 +181,29 @@ class ProfileDisplay: BaseVC {
         role.text = editRole.text
         bio.text = editBio.text
         
+        let b64Pic = EncodeImage(image: profilePic.image!)
+        
         let json: [String: Any] = [
-            "email":email.text!,
             "name" : name.text!,
             "role": role.text!,
             "location": location.text!,
             "bio":bio.text!,
-            "phone":phone.text!,
+            "phone":phone.text!
         ]
+        
+        let picJson: [String: Any] = [
+            "base64" : b64Pic
+        ]
+        
+        //EDIT PROFILE POST
         let response = post(action: "edit_profile", json: json,with: ["email": email.text!])
         print(response.statusCode!)
+        
+        //ADD PRO PIC POST
+        let picResp = post(action: "add_profile_picture", json: picJson,with:["email": email.text!])
+        print(picResp.statusCode!)
+        
+        update.isHidden = true
         
     }
     
